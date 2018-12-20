3cc6e59e281e008568e53b9c33f0511eb933e343
sin_een <sineen.nawatha@mail.huji.ac.il>
2018-12-20 18:41:21 +0200

    stuff

--- Matrix3D.cpp
+++ Matrix3D.cpp
@@ -33,3 +33,87 @@ Matrix3D::Matrix3D(Matrix3D& second)
 }
 
 
+Matrix3D& Matrix3D::operator+=(Matrix3D &other)
+{
+    this->firstRow+=other.firstRow;
+    this->secondRow+=other.secondRow;
+    this->thirdRow+=other.thirdRow;
+    return *this;
+}
+
+
+Matrix3D& Matrix3D::operator-=(Matrix3D &other)
+{
+    this->firstRow-=other.firstRow;
+    this->secondRow-=other.secondRow;
+    this->thirdRow-=other.thirdRow;
+    return *this;
+}
+
+Matrix3D& Matrix3D::operator=*(Matrix3D &other)
+{
+    return *Matrix3D(this*other);
+}
+
+Matrix3D Matrix3D::operator+(Matrix3D &other)
+{
+    return Matrix3D(this->firstRow + number, this,secondRow + number, this->thirdRow + number);
+
+//    double mul[3][3];
+//    for (int i = 0; i < 3; i ++)
+//    {
+//        for (int j = 0; j < 3; j ++)
+//        {
+//            mul[i][j] = this[i][j]+transposed[i][j];
+//        }
+//    }
+//    return Matrix3D(mul);
+}
+
+Matrix3D Matrix3D::operator-(Matrix3D &other)
+{
+    return Matrix3D(this->firstRow - number, this,secondRow - number, this->thirdRow - number);
+//    double mul[3][3];
+//    for (int i = 0; i < 3; i ++)
+//    {
+//        for (int j = 0; j < 3; j ++)
+//        {
+//            mul[i][j] = this[i][j]-transposed[i][j];
+//        }
+//    }
+//    return Matrix3D(mul);
+}
+
+Matrix3D Matrix3D::operator*(Matrix3D &other)
+{
+    double mul[3][3];
+    Vector3D firstColume = Vector3D(other.firstRow[0], other.secondRow[0], other.thirdRow[0]);
+    Vector3D secondColume = Vector3D(other.firstRow[1], other.secondRow[1], other.thirdRow[1]);
+    Vector3D thirdColume = Vector3D(other.firstRow[2], other.secondRow[2], other.thirdRow[2]);
+    Matrix3D transposed(firstColume,secondColume, thirdColume);
+    for (int i = 0; i < 3; i ++)
+    {
+        for (int j = 0; j < 3; j ++)
+        {
+            mul[i][j] = this[i]*transposed[j];
+        }
+    }
+    return Matrix3D(mul);
+}
+
+
+Matrix3D& Matrix3D::operator*=(double number)
+{
+    this->firstRow*=number;
+    this,secondRow*=number;
+    this->thirdRow*=number;
+}
+
+Matrix3D& Matrix3D::operator/=(double number)
+{
+    this->firstRow/=number;
+    this,secondRow/=number;
+    this->thirdRow/=number;
+}
+
+
